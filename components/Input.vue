27fa5ba9c8eefeaff6d0fd83f232398caf347c78
BingNi <nibing1220@qq.com>
2023-11-15 20:59:13 +0800

    init2

--- components/Input.vue
+++ components/Input.vue
@@ -27,7 +27,7 @@
         >
           <div class="flex items-center space-x-5"></div>
           <div class="flex-shrink-0">
-            <div class="tooltip" data-tip="Post Your Question">
+            <div class="tooltip" data-tip="Post Your Answer">
               <button
                 class="inline-flex items-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                 @click="handleSubmit"
