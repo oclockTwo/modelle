f33326f3100c3309c130e27035573a32d74f9705
BingNi <nibing1220@qq.com>
2023-11-16 14:03:53 +0800

    add warm tips

--- components/Modal.vue
+++ components/Modal.vue
@@ -4,9 +4,15 @@
   <dialog id="my_modal_1" class="modal">
     <div class="modal-box">
       <div v-if="isLoading" class="text-center mt-8">
-        <p class="font-bold text-lg mb-6">It's almost done... It's almost done... Don't rush it</p>
+        <p class="font-bold text-lg mb-6">
+          It's almost done... It's almost done... Don't rush it
+        </p>
         <span class="loading loading-spinner loading-lg"></span>
         <p class="text-6xl mt-6">🫣</p>
+        <p class="text-sm mt -4">
+          Warm tips: the more output content will be slower, usually about 10s
+          can be completed. If it takes too long to finish, you can re-post it.
+        </p>
       </div>
       <div class="text-center">
         <div v-if="isCorrect === 1">
@@ -18,9 +24,13 @@
         <div v-if="isCorrect === 2">
           <p class="font-bold text-lg mb-6">Oh no! Just a little bit</p>
           <p class="text-6xl">🥺</p>
-          <p class="text-center">Please check that your post and outputs meet the requirements.</p>
+          <p class="text-center">
+            Please check that your post and outputs meet the requirements.
+          </p>
           <p class="font-bold mt-6">Output: {{ output }}</p>
-          <p class="font-bold mt-6" v-if="explanation.length > 0">Explanation: {{ explanation }}</p>
+          <p class="font-bold mt-6" v-if="explanation.length > 0">
+            Explanation: {{ explanation }}
+          </p>
         </div>
       </div>
       <div class="modal-action">
