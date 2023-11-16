f1b27de32d554407da66c915c8cf14bc826979c0
BingNi <nibing1220@qq.com>
2023-11-17 03:49:41 +0800

    add introduction and faq

---
+++ components/Introduction.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <!-- Open the modal using ID.showModal() method -->
+    <!-- <button class="btn" onclick="my_modal_2.showModal()">open modal</button> -->
+    <dialog id="my_modal_2" class="modal">
+      <div class="modal-box">
+        <h2 class="font-bold text-lg text-center">How to play?</h2>
+        <p class="py-4">
+          It's a puzzle game that talks to AI large language models:<br>
+          <br>
+          1. First, read the questions carefully and understand the requirements
+          (sometimes it's the questions you don't understand that keep you from
+          passing). <br>
+          <br>
+          2. Post your answer, your answer is the input of the large
+          language model. <br>
+          <br>
+          3. Wait for some time (we would like to output the
+          result immediately, but this is determined by chatgpt) <br>
+          <br>
+          4. Look at the output, which is the output of the large language model. <br>
+          <br>
+          5. If you don't pass this question, see if the answer in your post fulfills the
+          requirements, and if the content of the output fulfills the answer to
+          the question.<br>
+          <br>
+          For more information you can check the FAQ below.
+        </p>
+
+        <div class="modal-action">
+          <form method="dialog">
+            <button class="btn">Close</button>
+          </form>
+        </div>
+      </div>
+    </dialog>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
--- pages/index.vue
+++ pages/index.vue
@@ -3,9 +3,31 @@
   <main
     class="h-screen overflow-auto font-Josefin-sans mx-auto max-w-3xl px-4 sm:px-6 lg:px-8"
   >
-    <h1 class="text-4xl text-center mt-4 bg-gray-200 rounded-md py-2">
-      Model<span class="text-green-400">Le</span> AI
-    </h1>
+    <div class="flex justify-between bg-gray-200 rounded-md items-center mt-4">
+      <div></div>
+      <h1 class="text-4xl text-center   py-2">
+        Model<span class="text-green-400">Le</span> AI
+      </h1>
+      <div class="pr-8">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          class="w-9 h-9"
+          @click="openIntroduction"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z"
+          />
+        </svg>
+      </div>
+    </div>
+
+    <Introduction />
     <Modal
       :is-loading="isLoading"
       @reset="reset()"
@@ -55,6 +77,7 @@
       </div>
     </div>
   </main>
+  <Faq />
 </template>
 
 <script setup>
@@ -171,6 +194,13 @@ function getOpenAPIData(data) {
     currentY = y.value;
   } else {
     isCorrect.value = 2;
+  }
+}
+
+function openIntroduction() {
+  const modal = document.getElementById("my_modal_2");
+  if (modal && modal.showModal) {
+    modal.showModal();
   }
 }
 
