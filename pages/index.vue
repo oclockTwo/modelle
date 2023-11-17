1eb3ebe3c7d54cc8e751b6b3fec18c05323cda36
BingNi <nibing1220@qq.com>
2023-11-17 15:17:38 +0800

    add zh

--- pages/index.vue
+++ pages/index.vue
@@ -4,7 +4,22 @@
     class="h-screen overflow-auto font-Josefin-sans mx-auto max-w-3xl px-4 sm:px-6 lg:px-8"
   >
     <div class="flex justify-between bg-gray-200 rounded-md items-center mt-4">
-      <div class="w-1/3"></div>
+      <div class="w-1/3">
+        <div class="dropdown dropdown-hover pl-4">
+          <label tabindex="0" class="btn btn-primary btn-sm m-1">Lang</label>
+          <ul
+            tabindex="0"
+            class="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52"
+          >
+            <li @click="changeLanuage('en')" class="text-md">
+              <a>english</a>
+            </li>
+            <li @click="changeLanuage('zh')" class="text-md">
+              <a>中文简体</a>
+            </li>
+          </ul>
+        </div>
+      </div>
       <h1 class="text-4xl text-center py-2">
         Model<span class="text-green-400">Le</span> AI
       </h1>
@@ -24,20 +39,20 @@
             d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z"
           />
         </svg>
-        
+
         <a
           href="https://forms.gle/QztDnd9b7ZTgP3pa7"
           target="_blank"
           rel="noreferrer"
           class="cursor-pointer pl-6"
-          >
+        >
           <div class="tooltip" data-tip="Submit your question">
-          <img
-            src="/images/icons/submit.jpg"
-            alt="twitter"
-            class="block w-8 max-w-full h-8 align-middle"
-        /></div></a>
-        
+            <img
+              src="/images/icons/submit.jpg"
+              alt="twitter"
+              class="block w-8 max-w-full h-8 align-middle"
+            /></div
+        ></a>
       </div>
     </div>
 
@@ -56,11 +71,12 @@
         </figure>
         <div class="card-body">
           <h2 class="text-center text-2xl">Chapter{{ x + 1 }}</h2>
+
           <p class="text-2xl text-center mt-2">
             Question{{ x + 1 }}-{{ y + 1 }}:
           </p>
           <p class="sm:text-sm md:text-xl text-center mt-2 h-24 overflow-auto">
-            {{ questions[x].value[y + 1].en }}
+            {{ questions[x].value[y + 1][lang] }}
           </p>
           <Input @sendOpenAPIData="getOpenAPIData" />
         </div>
@@ -217,6 +233,11 @@ function openIntroduction() {
   if (modal && modal.showModal) {
     modal.showModal();
   }
+}
+
+const lang = ref("en");
+function changeLanuage(langString) {
+  lang.value = langString;
 }
 
 useHead({
